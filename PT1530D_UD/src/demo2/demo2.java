7b414be6ba6cc499b78b7d5d081af7035f1454c1
minhdq8 <50552420+minhdq8@users.noreply.github.com>
2020-03-19 10:48:16 +0700

    Add files via upload

--- PT1530D_UD/src/demo2/demo2.java
+++ PT1530D_UD/src/demo2/demo2.java
@@ -5,12 +5,59 @@
  */
 package demo2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author minhdq
  */
 public class demo2 {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         
+        nhapXuatMang();
+    }
+
+    public static void nhapXuatMang() {
+
+        // b1 nhập kích thước mảng
+        System.out.print("Mời nhập size của mảng: ");
+        int size = Integer.parseInt(sc.nextLine());
+        // b2 khai báo và khởi tạo mảng
+        int tuoi[] = new int[size];
+        // b3 Nhập vào mảng
+        System.out.println("Nhập vào từng phần tử của mảng");
+        for (int i = 0; i < size; i++) {
+            System.out.printf("Phần tử thứ %d: ", i + 1);
+            tuoi[i] = sc.nextInt();
+        }
+        // b4 xuất mảng
+        System.out.print("\nMảng vừa nhập là: ");
+        for (int i = 0; i < tuoi.length; i++) {
+            System.out.print(tuoi[i] + "  ");
+        }
+    }
+
+    public static void TBC() {
+        /*
+        9. Tính trung bình cộng các số chia 
+        hết cho 3 từ 27 đến 35 // 27 30 33 -> tbc 30
+         */
+        int tong = 0;
+        int soLuong = 0;
+
+        int min = sc.nextInt();
+        int max = sc.nextInt();
+        for (int i = min; min <= max; i++) {
+            if (i % 3 == 0) {
+                tong += i; // tong = tong + i
+                soLuong++;
+            }
+        }
+        double TBC = tong / soLuong;
+        System.out.println(TBC);
+
     }
 }
